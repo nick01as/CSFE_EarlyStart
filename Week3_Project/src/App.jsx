bf2b9a32fbf3781861209aa26423f27f3cc6501c
Nicholas <77217356+nick01as@users.noreply.github.com>
2024-03-08 21:53:52 -0800

    Add files via upload

---
+++ Week3_Project/src/App.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+import { NewTodoForm } from "./NewTodoForm"
+import "./styles.css"
+import { TodoList } from "./TodoList"
+
+export default function App() {
+  const [newCounter, setNewCounter] = useState(0)
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false},
+      ]
+    })
+  }
+
+  function toggleTodo(id, completed) {
+    setNewCounter(newCounter + 1)
+    deleteTodo(id)
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h3>Completed Tasks: {newCounter}</h3>
+      <h3>Pending Tasks: {todos.length}</h3>
+      <br/>
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} newCounter={newCounter}/>
+    </>
+  )
+}
